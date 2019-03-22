e210173c17f17cb5ea7a0de9f09324a247cd5217
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-03-22 10:26:51 -0500

    done

--- github.js
+++ github.js
@@ -2,13 +2,19 @@ class Github {
   constructor() {
     this.client_id = '6d031e666aaff36684c2';
     this.client_secret = 'd52eba75e6dfbea73ab24938707e81b85a1392a0';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -27,8 +27,30 @@ class UI {
       </div>
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
-      <div id="repos></div>
+      <div id="repos"></div>
   `;
+  }
+
+  showRepos(repos) {
+    let output = '';
+    repos.forEach((repo)=>{
+      output += `
+      <div class="card card-body mb-2>
+      <div class="row">
+      <div class="col-md-6">
+      <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+      </div>
+      <div class="col-md-6">
+      <span class="badge badge-secondary">Stars: ${repo.stargazers_count}</span>
+      <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+      <span class="badge badge-secondary">Forks: ${repo.forks_count}</span>
+      </div>
+      </div>
+      </div>
+      `;
+    })
+
+    document.getElementById('repos').innerHTML = output;
   }
 
   showAlert(message, className) {
@@ -41,7 +63,6 @@ class UI {
   container.insertBefore(div, search);
   setTimeout(() => {
     this.clearAlert();
-
   }, 3000);
   }
 
